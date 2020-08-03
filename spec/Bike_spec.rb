13c84270ea8cb77e6b49200cdac09e51df03c463
Olatunji Oduola <ola.oduola@gmail.com>
2020-08-03 17:46:13 +0100

    Green: working method returns true

--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -4,5 +4,9 @@ describe Bike do
   
   context '#working?' do
     it { is_expected.to respond_to(:working?) }
+
+    it 'should return true' do
+      expect(subject.working?).to be(true)
+    end
   end
 end
