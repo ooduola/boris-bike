7bb214f1baa8e0338ec846ca1c74eb874f0ce785
Olatunji Oduola <ola.oduola@gmail.com>
2020-08-03 17:13:36 +0100

    Refactor: release_bike test written in one-liner syntax

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,9 +3,7 @@ require 'DockingStation'
 describe DockingStation do
 
   context '#release_bike' do
-    it 'responds to release bike' do
-      expect(DockingStation.new).to respond_to(:release_bike)
-    end
+    it { is_expected.to respond_to(:release_bike) }
   end
   
 end
