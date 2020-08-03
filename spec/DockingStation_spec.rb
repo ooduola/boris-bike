41f5fab5101ddb292d79a612ba5799b6e5efae2d
Olatunji Oduola <ola.oduola@gmail.com>
2020-08-03 17:11:03 +0100

    Green: release_bike method defined

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,5 +1,11 @@
 require 'DockingStation'
 
 describe DockingStation do
+
+  context '#release_bike' do
+    it 'responds to release bike' do
+      expect(DockingStation.new).to respond_to(:release_bike)
+    end
+  end
   
 end
