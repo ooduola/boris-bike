926500cbe63b5b64fd4f433e67d3eaa0d239a188
Olatunji Oduola <ola.oduola@gmail.com>
2020-08-04 12:20:22 +0100

    Green. release bike releases bike which has been docked

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,11 +4,10 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    @bike
   end
 
   def dock(bike)
     @bike = bike
   end
-  
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,12 +4,23 @@ describe DockingStation do
 
   context '#release_bike' do
     it { is_expected.to respond_to(:release_bike) }
+
+    it 'releases docked bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    # it 'raises an error if no bikes are available' do
+    #   expect { subject.release_bike }.to raise_error 
+    # end
+
+    it 'should return a bike' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
   end
 
-  it 'should return a bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
 
   context '#dock' do
     it { is_expected.to respond_to(:dock).with(1).argument }
