72b4efd0bb5719ec6c21396950a2f51eb4348f2b
Olatunji Oduola <ola.oduola@gmail.com>
2020-08-09 20:20:03 +0100

    Green. Bike can be reported broken and confirm its broken

--- lib/Bike.rb
+++ lib/Bike.rb
@@ -1,7 +1,14 @@
-class Bike 
+class Bike
 
   def working?
     true
   end
 
+  def report_broken
+  end
+
+  def broken?
+
+    true
+  end
 end
--- spec/Bike_spec.rb
+++ spec/Bike_spec.rb
@@ -5,4 +5,10 @@ describe Bike do
   context '#working?' do
     it { is_expected.to respond_to(:working?) }
   end
+
+  it 'can be reported broken' do
+    subject.report_broken
+    expect(subject).to be_broken
+  end
+
 end
